bb02f4d69a0c1b00c20442d953dd56ce5f9d4adb
Yi Wang <bt20001@qmul.ac.uk>
2021-06-08 17:56:39 +0100

    update result with drop_size 0

--- shell/htseq_samtools/run_mapping.sh
+++ shell/htseq_samtools/run_mapping.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/shell
 
 size=$1
-python ../../script/pipeline.py -b ../../data/Alignment/ -g /data/BCI-Haemato/Yi/analysis/eRNA_project/05.extracted/$size/extracted_$size".gff" -r /data/BCI-Haemato/Yi/analysis/eRNA_project/05.extracted/$size/extracted_$size".bed" -o /data/BCI-Haemato/Yi/analysis/eRNA_project/06.samtools_htseq
+wkdir=$2
+
+if [ -z $size ];then
+	echo "sh run_mapping.sh [size] [wkdir]"
+elif [ -z $wkdir ];then
+	echo "sh run_mapping.sh [size] [wkdir]"
+else
+	mkdir -p $wkdir/06.samtools_htseq/$size 
+	python $wkdir/script/pipeline_v2.py -b $wkdir/data/Alignment/ -g $wkdir/05.extracted/$size/extracted_$size".gff" -r $wkdir/05.extracted/$size/extracted_$size".bed" -o $wkdir/06.samtools_htseq/$size
+fi
