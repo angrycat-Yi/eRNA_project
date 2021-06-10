c23d700229308f19e636d39f168b1b62d7f22381
Yi Wang <bt20001@qmul.ac.uk>
2021-06-10 22:42:48 +0100

    change directory structure

--- shell/htseq_samtools/run_mapping.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/shell
-
-size=$1
-mkdir -p /data/BCI-Haemato/Yi/analysis/eRNA_project/06.samtools_htseq/$size 
-python /data/BCI-Haemato/Yi/analysis/eRNA_project/script/pipeline_v2.py -b /data/BCI-Haemato/Yi/data/Alignment/ -g /data/BCI-Haemato/Yi/analysis/eRNA_project/05.extracted/$size/extracted_$size".gff" -r /data/BCI-Haemato/Yi/analysis/eRNA_project/05.extracted/$size/extracted_$size".bed" -o /data/BCI-Haemato/Yi/analysis/eRNA_project/06.samtools_htseq/$size
